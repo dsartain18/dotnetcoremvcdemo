3c9edd7055fd177cebfc60bb22dad6f53245bb13
Don Sartain <c102915@gamestop.com>
2017-09-25 11:09:17 -0500

    Renamed Program.cs to LocalEntryPoint.cs. Added LambdaEntryPoint.cs

---
+++ WebApiRdsDemoMREPattern/WebApplication2/LambdaEntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApplication2
+{
+    public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<DependencyResolution.Startup>()
+                .UseSetting(WebHostDefaults.ApplicationKey, typeof(LambdaEntryPoint).GetTypeInfo().Assembly.FullName) // Ignore the startup class assembly as the "entry point" and instead point it to this app
+               .UseApiGateway(); 
+        }
+    }
+}
